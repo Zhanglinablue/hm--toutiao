6be3c6b51826b6760e8c243d3e52a4637e2d3b8e
lisi <1411452095>
2020-01-29 18:03:16 +0800

    使用vant

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vant/lib/index.css'
+import Vant from 'vant'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
