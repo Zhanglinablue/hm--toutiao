44dff03730059b75766363f41c42357135258fdd
lisi <1411452095>
2020-02-03 16:25:45 +0800

    引入全局样式覆盖vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'vant/lib/index.css'
+// import 'vant/lib/index.css'
+import 'vant/lib/index.css' // vant 样式文件
+import '@/styles/index.less' // 引入全局的自定义样式  因为要覆盖vant的样式
 import Vant from 'vant'
 import 'amfe-flexible'
 Vue.use(Vant)
