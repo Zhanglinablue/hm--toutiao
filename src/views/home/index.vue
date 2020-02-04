ada9bd39d7a53f3c54bb95cf54aec0e4b6549b62
lisi <1411452095>
2020-02-04 17:00:07 +0800

    渲染真实的频道

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='container'>
     <van-tabs v-model="activeIndex" swipeable>
-      <van-tab :title="'标签' + item" v-for="item in 10" :key="item">
+      <van-tab :title="channel.name" v-for="channel in channels" :key="channel.id">
    <article-list></article-list>
       </van-tab>
     </van-tabs>
@@ -13,15 +13,26 @@
 
 <script>
 import ArticleList from './components/article-list'
+import { getMyChannels } from '@/api/channels'
 export default {
   name: 'home',
   data () {
     return {
-      activeIndex: 0
+      activeIndex: 0,
+      channels: []
     }
   },
   components: {
     ArticleList
+  },
+  methods: {
+    async getMyChannels () {
+      let data = await getMyChannels()
+      this.channels = data.channels
+    }
+  },
+  created () {
+    this.getMyChannels()
   }
 }
 </script>
