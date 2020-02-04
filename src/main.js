e787ba84f6fd1a00c1912943c94fd60d98137c02
lisi <1411452095>
2020-02-04 14:04:15 +0800

    提交主题颜色修改

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './permission'
 import store from './store'
 // import 'vant/lib/index.css'
+import 'vant/lib/index.less' // vant 样式文件 换成less
 import 'vant/lib/index.css' // vant 样式文件
 import '@/styles/index.less' // 引入全局的自定义样式  因为要覆盖vant的样式
 import Vant from 'vant'
