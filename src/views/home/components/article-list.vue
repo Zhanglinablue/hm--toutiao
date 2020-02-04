ed0a1b0efa28dee11f4fcb074ddbf98b2fb5937b
lisi <1411452095>
2020-02-04 16:09:38 +0800

    article-list实现上拉加载

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="scroll-wrapper">
-      <van-list v-model="upLoading" :finished="finished" finished-text="没有了" @load="onLoad"></van-list>
+      <van-list
+      v-model="upLoading"
+      :finished="finished"
+      finished-text="没有了"
+       @load="onLoad">
+       <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
+       </van-list>
   </div>
 </template>
 
@@ -10,12 +16,22 @@ export default {
   data () {
     return {
       upLoading: false,
-      finished: false
+      finished: false,
+      articles: []
     }
   },
   methods: {
     onLoad () {
       console.log('开始加载数据')
+      setTimeout(() => {
+        if (this.articles.length < 50) {
+          let arr = Array.from(Array(10), (value, index) => this.articles.length + index + 1)
+          this.articles.push(...arr)
+          this.upLoading = false
+        } else {
+          this.finish = true
+        }
+      }, 1000)
     }
   }
 }
